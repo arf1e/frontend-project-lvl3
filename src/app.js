8a8f676dcf0335375afbba283e038a65c7a8be08
arf1e <anotheruselessbox@gmail.com>
2021-12-14 10:58:39 +0300

    task 5

--- src/app.js
+++ src/app.js
@@ -1,11 +1,40 @@
+import _ from 'lodash';
 import i18next from 'i18next';
 import proxyClient from './client';
 import createWatchedState from './view';
 import validate from './validate';
 import ru from './locales/ru';
-import FORM_STAGES from './constants';
+import FORM_STAGES, { FEED_UPDATE_TIMER } from './constants';
 import parseRssFeed from './parser';
 import { getElements } from './dom';
+
+const setFeedUpdater = (watchedState, updatePosts) => {
+  const timeoutId = setTimeout(() => {
+    const { feeds, posts } = watchedState;
+
+    const feedUrls = feeds.map((feed) => feed.url);
+    // prettier-ignore
+    const connections = feedUrls.map((url) => proxyClient.get(url).then(({ data }) => {
+      const newPosts = parseRssFeed(data.contents, url).posts;
+      return newPosts;
+    }));
+
+    Promise.all(connections)
+      .then((newPosts) => {
+        const difference = _.differenceWith(posts, newPosts, (a, b) => a.title !== b.title);
+        if (!_.isEmpty(difference)) {
+          updatePosts(difference);
+        }
+      })
+      .finally(() => {
+        setFeedUpdater(watchedState, updatePosts);
+      })
+      .catch(() => {
+        // TODO: Handle error
+        clearTimeout(timeoutId);
+      });
+  }, FEED_UPDATE_TIMER);
+};
 
 const app = () => {
   const state = {
@@ -24,6 +53,11 @@ const app = () => {
     ...elements,
   });
 
+  const updatePosts = (posts) => {
+    watchedState.posts = [...posts, ...watchedState.posts];
+  };
+  setFeedUpdater(watchedState, updatePosts);
+
   form.addEventListener('submit', (e) => {
     e.preventDefault();
     const url = e.target.url.value;
@@ -40,7 +74,7 @@ const app = () => {
         return stream;
       })
       .then((rss) => {
-        const { feed, posts } = parseRssFeed(rss);
+        const { feed, posts } = parseRssFeed(rss, url);
         const feedExists = watchedState.feeds.find((element) => element.title === feed.title);
         if (feedExists) throw new Error(i18next.t('errors.urlAlreadyExists'));
         return { feed, posts };
