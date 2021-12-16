5e7a894140a27c2ceb02f2e86250cbb0f68c9daf
arf1e <anotheruselessbox@gmail.com>
2021-12-16 11:59:34 +0300

    hex check

--- src/app.js
+++ src/app.js
@@ -67,16 +67,13 @@ const app = (state) => {
 };
 
 export default (state) => {
-  document.addEventListener('DOMContentLoaded', () => {
-    console.log('loaded');
-    i18next
-      .init({
-        lng: 'ru',
-        debug: true,
-        resources: {
-          ru,
-        },
-      })
-      .then(app(state));
-  });
+  i18next
+    .init({
+      lng: 'ru',
+      debug: true,
+      resources: {
+        ru,
+      },
+    })
+    .then(() => app(state));
 };
